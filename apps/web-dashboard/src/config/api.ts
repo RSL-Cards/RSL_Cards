1934a69e86732208883660a6ee05fc786262741d
Vinay Kumar Golla <gollavinay13@gmail.com>
2026-07-07 15:01:13 +0530

    api configured in next js

--- apps/web-dashboard/src/config/api.ts
+++ apps/web-dashboard/src/config/api.ts
@@ -1,6 +1,5 @@
-// export const API_BASE_URL =
-//   process.env.NEXT_PUBLIC_API_BASE_URL ?? 'http://3.231.19.101'
-export const API_BASE_URL = "http://localhost:8080"
+export const API_BASE_URL =
+  process.env.NEXT_PUBLIC_API_BASE_URL ?? 'http://3.231.19.101'
 
 export const ENDPOINTS = {
   auth: {
